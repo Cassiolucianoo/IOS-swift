524ccbda01d362c24ba950692a471e44c171de87
cassio <cassiolucianodasilva@gmail.com>
2021-08-30 18:32:31 -0300

    Utilizar o playground para aprender a sintaxe da linguagem Swift Fazer comentários Utilizar funções

---
+++ bank/MyPlayground.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+//tipos de comentarios
+
+//basico
+/*
+ varias linhas
+ */
+//comentar em todas as linhas command / selecionando o cod
+
+
+var greeting = "Hello, playground"
+
+var nome = "Casso Luciano da Silva"
+
+nome = "Luciano"
+
+var endereco = "São Paulo"
+
+print(nome)
+
+//declarando valores
+
+let nomet = "Churros"
+let tristezas: Int = 5
+let calorias: Double = 79.5
+
+
+func add(){
+    
+    print("Teste")
+    
+}
+func add(opa: String){
+    
+    print("Teste")
+    
+}
+
+var opa = "Opa"
+
+add(opa: opa)
+add()
+
+
+
+
+
