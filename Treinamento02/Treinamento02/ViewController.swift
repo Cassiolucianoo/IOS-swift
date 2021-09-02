c17fcba43d75fa8f6a0ac3ad6f87ae5c8c4fc969
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2021-09-02 14:43:37 -0300

    TableView
    Implementar o componente visual TableView;
    Conectar a TableView ao ViewController;
    Métodos obrigatórios para implementação da TableView.

---
+++ Treinamento02/Treinamento02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treinamento02
+//
+//  Created by Virtual Machine on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
