1cc3c03165c65df9c72c7201e35c727383822d9f
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2021-09-28 18:26:40 -0300

    Layout Conversores
    Ajustes

---
+++ Treinamento1.02 - Conversores/Treinamento1.02 - Conversores/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Treinamento1.02 - Conversores
+//
+//  Created by Virtual Machine on 28/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    @IBAction func SHOWnEXT(_ sender: Any) {
+    }
+    
+    @IBAction func convert(_ sender: Any) {
+    }
+    
+    
+    
+    
+}
+
