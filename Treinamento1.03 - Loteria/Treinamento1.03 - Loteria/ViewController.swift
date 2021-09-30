7234ae823441e50207e5fdaba7d946dc2ffa2051
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2021-09-30 15:42:09 -0300

    App - Loteria Treinamento1.03
    - usando enum
    - Funcoes
    - Controle de fluxo
    - Closures

--- Treinamento1.03 - Loteria/Treinamento1.03 - Loteria/ViewController.swift
+++ Treinamento1.03 - Loteria/Treinamento1.03 - Loteria/ViewController.swift
@@ -7,17 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum GameType: String {
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
 
+infix operator >-<
+func >-< (total: Int, universe: Int)-> [Int]{
+    var result : [Int] = []
+    while result.count < total {
+        let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+        if !result.contains(randomNumber){
+            result.append(randomNumber)
+        }
+    }
+    return result.sorted()
+}
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scgameType: UISegmentedControl!
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet var balls: [UIButton]!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Array - Coleções ordenada de objetos de mesmo tipo
-        // coleção apenas do mesmo tipo
-        
-        
+        showNumbers(for: .megasena)
         
     }
-
-
+    
+    func showNumbers (for type: GameType){
+        lbGameType.text = type.rawValue
+        var game: [Int] = []
+        switch type {
+        case .megasena:
+            game = 6>-<60
+            balls.last!.isHidden = false
+        case .quina:
+            game = 5>-<80
+            balls.last!.isHidden = true
+        }
+        for (index, game) in game.enumerated(){
+            balls[index].setTitle("\(game)", for: .normal)
+        }
+        
+    }
+    
+    @IBAction func generateGame() {
+        switch scgameType.selectedSegmentIndex{
+        case 0:
+            showNumbers(for: .megasena)
+        default:
+            showNumbers(for: .quina)
+        }
+    }
+    
+    
 }
 
