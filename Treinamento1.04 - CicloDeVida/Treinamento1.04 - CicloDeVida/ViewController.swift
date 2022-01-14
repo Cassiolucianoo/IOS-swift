24c9b1d9442ce7b86efef82bc25c6ecdf9734cee
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-01-14 01:01:11 -0300

    AppDelegate
    conhecendo Ciclo de Vida

---
+++ Treinamento1.04 - CicloDeVida/Treinamento1.04 - CicloDeVida/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treinamento1.04 - CicloDeVida
+//
+//  Created by cassio luciano on 13/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
