6ebf4dc7e29c4d9eb4e9105c011cee06e5e9cd61
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-01-21 09:04:08 -0300

    Views Outles Actions
    Navegando entre as Views

---
+++ Treinamento1.07 - ViewsOutletsActions/Treinamento1.07 - ViewsOutletsActions/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Treinamento1.07 - ViewsOutletsActions
+//
+//  Created by cassio luciano on 19/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet var Buttons: [UIButton]!
+    var nome: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
