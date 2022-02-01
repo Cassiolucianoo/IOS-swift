59a8d484097c988674f9a0494f010b9363d54e95
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-01-31 22:58:27 -0300

    App senha secreta
    - Criando view geradora de senha com constraints

---
+++ Treinamento1.09 - SenhaBruta/Treinamento1.09 - SenhaBruta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treinamento1.09 - SenhaBruta
+//
+//  Created by cassio luciano on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
