fe04c0ec06c81f9f74e31439f0d26524f817845e
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-02-07 23:54:09 -0300

    Treinamento
    Array gerador de senhas

--- Treinamento1.09 - SenhaBruta/Treinamento1.09 - SenhaBruta/GeradorDeSenhas.swift
+++ Treinamento1.09 - SenhaBruta/Treinamento1.09 - SenhaBruta/GeradorDeSenhas.swift
@@ -20,12 +20,16 @@ class GeradorDeSenhas {
     
     private let letras = "abcdefghijklmnopqrstuvwxyz"
     private let numeros = "0123456789"
-   private let caracteresEspeciais = ""
+   private let caracteresEspeciais = "!@#$%&*()_-+=~`|]][{':;?/<>.,"
 
 
 
     init( numeroDeLetras: Int , usarLetrasMinusculas: Bool , usarLetrasMaiusculas: Bool, usarCaracteresEspeciais: Bool, usarNumero: Bool) {
-        self.numeroDeLetras = numeroDeLetras
+        
+        var numerosCaracteres = min( numeroDeLetras, 16)
+        numerosCaracteres = max( numerosCaracteres, 1)
+        
+        self.numeroDeLetras = numerosCaracteres
         self.usarLetrasMinusculas = usarLetrasMinusculas
         self.usarLetrasMaiusculas = usarLetrasMaiusculas
         self.usarCaracteresEspeciais = usarCaracteresEspeciais
@@ -33,4 +37,45 @@ class GeradorDeSenhas {
     }
     
     
+    func gerador(total: Int) -> [String] {
+        
+        senhas.removeAll()
+        
+        var usar: String = ""
+        
+        if usarCaracteresEspeciais{
+            
+            usar += caracteresEspeciais
+            
+        }
+        if usarNumero{
+            
+            usar += numeros
+            
+        }
+        if usarLetrasMinusculas{
+            
+            usar += letras
+            
+        }
+        if usarLetrasMaiusculas{
+            
+            usar += letras.uppercased()
+            
+        }
+        
+        let usarArray = Array(usar)
+        while senhas.count < total {
+            var senha = ""
+            for _ in 1...numeroDeLetras{
+                let index = Int(arc4random_uniform(UInt32(usarArray.count)))
+                senha += String(usarArray[index])
+            }
+            senhas.append(senha)
+        }
+        
+        
+        return senhas
+        
+    }
 }
