a13e22f437ab453adc3fb0d71646277735d34fbb
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-02-09 23:05:10 -0300

    Treinamento 11
    Tabelas - UiTableView

---
+++ Treinamento1.11 - Campeoes da Compa do Mundo/Treinamento1.11 - Campeoes da Compa do Mundo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treinamento1.11 - Campeoes da Compa do Mundo
+//
+//  Created by cassio luciano on 09/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
