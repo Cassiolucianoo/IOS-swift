4619859efa426f733313c1f4b6e94c99c37535f1
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-02-10 22:45:52 -0300

    Treinamento 11 parte 2
    Organização
    - model
    - view
    - controller

---
+++ Treinamento1.11 - Campeoes da Compa do Mundo/Treinamento1.11 - Campeoes da Compa do Mundo/Controllers/CampeosCopaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CampeosCopaViewController.swift
+//  Treinamento1.11 - Campeoes da Compa do Mundo
+//
+//  Created by cassio luciano on 10/02/22.
+//
+
+import UIKit
+
+class CampeosCopaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Treinamento1.11 - Campeoes da Compa do Mundo/Treinamento1.11 - Campeoes da Compa do Mundo/Model/CopaDoMundo.swift
@@ -0,0 +1,8 @@
+//
+//  CopaDoMundo.swift
+//  Treinamento1.11 - Campeoes da Compa do Mundo
+//
+//  Created by cassio luciano on 10/02/22.
+//
+
+import Foundation
