9ca6e7790d1825666d37f7c3b13179656bd63781
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-07-04 07:13:51 -0300

    MoviesList
    Iniciando projeto

---
+++ Teste/Teste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste
+//
+//  Created by cassio luciano on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
