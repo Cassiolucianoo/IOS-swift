fb9497021d9519f4d20e044ea3ce8dda26f7fbde
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-07-25 08:38:40 -0300

    storyboard
    Navegação entre view

---
+++ Navegação/storyboard/push/ViewControllerAzul.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  push
+//
+//  Created by cassio luciano on 25/07/22.
+//
+
+import UIKit
+
+class ViewControllerAzul: UIViewController {
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Azul"
+        
+    }
+    
+}
---
+++ Navegação/storyboard/push/ViewControllerRosa.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  push
+//
+//  Created by cassio luciano on 25/07/22.
+//
+
+import UIKit
+
+class ViewControllerRosa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = " Rosa "
+    }
+    
+
+}
