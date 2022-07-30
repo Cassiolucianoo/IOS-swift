be34eb20efe9eefa91a1c1edabd4508417aaa531
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2022-07-30 09:05:22 -0300

    MKAnnotation
    - adicionando pontos no mapa com MKAnnotation
    - Limpando tabela com  UITableViewCell.EditingStyle

--- Treinamento1.12- Manipulando mapas/Treinamento1.12- Manipulando mapas/MapaViewController.swift
+++ Treinamento1.12- Manipulando mapas/Treinamento1.12- Manipulando mapas/MapaViewController.swift
@@ -14,10 +14,37 @@ class MapaViewController: UIViewController {
     @IBOutlet weak var searchBarPesquisa: UISearchBar!
     @IBOutlet weak var lbDescricaoLocal: UILabel!
     @IBOutlet weak var lbNomeLocal: UILabel!
+    @IBOutlet weak var viInfo: UIView!
+    
+    var places: [Place]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        searchBarPesquisa.isHidden = true
+        viInfo.isHidden = true
+        
+        if places.count == 1 {
+            title = places[0].nome
+        } else {
+            title = " Meus lugares"
+        }
+        
+        for place in places {
+            addToMap(place)
+        }
+         showPlaces()
+    }
+    
+    func addToMap(_ place : Place){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = place.coordenada
+        annotation.title = place.nome
+        mpView.addAnnotation(annotation)
+        
+    }
+    
+    func showPlaces(){
+        mpView.showAnnotations(mpView.annotations, animated: true)
     }
     
     @IBAction func direcionarrota(_ sender: Any) {
--- Treinamento1.12- Manipulando mapas/Treinamento1.12- Manipulando mapas/PlacesTableViewController.swift
+++ Treinamento1.12- Manipulando mapas/Treinamento1.12- Manipulando mapas/PlacesTableViewController.swift
@@ -12,16 +12,30 @@ class PlacesTableViewController: UITableViewController {
     //array para popular a tabela
     var places: [Place] = []
     let ud = UserDefaults.standard
+    var lbNoPlaces: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadPlaces()
+        
+        lbNoPlaces = UILabel()
+        lbNoPlaces.text = "Cadastre os locais que deseja conhcer\nclicando no botão + acima"
+        lbNoPlaces.textAlignment = .center
+        lbNoPlaces.numberOfLines = 0
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier! != "Segue"{
+        if segue.identifier! != "mapSegue"{
             let vc = segue.destination as! PlaceFinderViewController
             vc.delegate = self
+        }else {
+            let vc = segue.destination as! MapaViewController
+            switch sender {
+            case let place as Place:
+                vc.places = [place]
+            default:
+                vc.places = places
+            }
         }
     }
 
@@ -41,11 +55,36 @@ class PlacesTableViewController: UITableViewController {
         ud.set(json, forKey: "places")
     
     }
+    @objc func showAll(){
+        performSegue(withIdentifier: "mapSegue", sender: nil)
+    }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if places.count > 0 {
+            let btShowall = UIBarButtonItem(title: "Mostrar todos no mapa", style: .plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btShowall
+            tableView.backgroundView = nil
+        }else {
+            navigationItem.leftBarButtonItem = nil
+            tableView.backgroundView = lbNoPlaces
+        }
+        
         return places.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = places[indexPath.row]
+        performSegue(withIdentifier: "mapSegue", sender: place)
+    }
+    // MARK: - editingStyle deletar da tabela conteudo
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            places.remove(at: indexPath.row)
+            tableView.deleteRows(at:[indexPath], with: .fade)
+            savePlace()
+        }
     }
 
   
