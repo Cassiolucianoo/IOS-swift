43e4ab285339b806f0d9b80709eab03409582863
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-01-11 10:13:47 -0800

    Componentes adicionados

--- CursoIOS - SwiftUi/SwiftUI-Previsao-Do-Tempo/SwiftUI-Previsao-Do-Tempo/ContentView.swift
+++ CursoIOS - SwiftUi/SwiftUI-Previsao-Do-Tempo/SwiftUI-Previsao-Do-Tempo/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [.blue, Color( "azulMaisClaro")]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack{
                 Text("São Paulo, BR")
@@ -26,8 +26,27 @@ struct ContentView: View {
                     Text("42º")
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
+                }.padding(.bottom, 40)
+                
+                HStack(spacing: 20){
+                    SemanasView(semana: "SEG", img:  "cloud.sun.fill", temp: 9 )
+                    SemanasView(semana: "TER", img:  "sun.max.fill", temp: 12)
+                    SemanasView(semana: "QUA", img:  "cloud.sun.fill", temp: 31)
+                    SemanasView(semana: "QUI", img:  "sunset.fill", temp: 32)
+                    SemanasView(semana: "SEX", img:  "wind.snow", temp: 12)
                 }
                 Spacer()
+                
+                Button{
+                    print("botão selecionado")
+                }
+                label:{
+                    Text("Mudar horario do Dia")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size:20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }.padding(.bottom, 70)
             }
         }
        
@@ -37,5 +56,27 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SemanasView: View {
+    var semana: String
+    var img: String
+    var temp: Int
+    
+    var body: some View {
+        VStack{
+            Text(semana)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: img)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temp)º")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
