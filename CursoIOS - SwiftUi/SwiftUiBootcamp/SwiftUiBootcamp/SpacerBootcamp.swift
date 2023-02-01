34676179d74762bbae335e75fc0b772d92b13819
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-02-01 20:47:19 -0300

    Um pouco sobre spacer init e enum

--- CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/SpacerBootcamp.swift
+++ CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/SpacerBootcamp.swift
@@ -13,7 +13,7 @@ struct SpacerBootcamp: View {
             Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
                 .frame(height: 10)
                 .background(Color.orange)
-                
+            
             Rectangle()
                 .frame(width: 50, height: 50)
             
@@ -47,8 +47,29 @@ struct SpacerBootcamp: View {
             
         }
         .background(Color.blue)
-    }
-}
+        
+        VStack{
+            HStack(){
+                Image(systemName: "xmark")
+                Spacer()
+                    .frame(height: 10)
+                    .background(Color.orange)
+                
+                Image(systemName: "xmark")
+            }
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .background(Color.yellow)
+            .padding(.horizontal)
+            .background(Color.orange)
+            
+            Spacer()
+                .frame(width: 10)
+                .background(Color.orange)
+            
+            Rectangle()
+                .frame(height: 55)
+        }
+    }}
 
 struct SpacerBootcamp_Previews: PreviewProvider {
     static var previews: some View {
