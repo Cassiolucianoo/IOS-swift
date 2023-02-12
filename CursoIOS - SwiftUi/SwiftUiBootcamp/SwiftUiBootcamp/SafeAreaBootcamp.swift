b5c7ccb1840de56a15f2ca0e6a55fb684d7c8c8a
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-02-12 20:31:15 -0300

    Safe Area Com edgesIgnoringSafeArea

---
+++ CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/SafeAreaBootcamp.swift
@@ -0,0 +1,40 @@
+//
+//  SafeAreaBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by cassio on 12/02/23.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        
+        
+        ScrollView{
+            VStack {
+                Text("Titulo")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                ForEach(0..<10) { index in
+                    
+                    RoundedRectangle(cornerRadius:  25.0)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .padding(20)
+                }
+            }
+        }
+        .background(
+            Color.red
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        )
+    }
+}
+
+struct SafeAreaBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SafeAreaBootcamp()
+    }
+}
