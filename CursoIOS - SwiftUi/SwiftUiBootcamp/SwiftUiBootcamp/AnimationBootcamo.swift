2c1c60dfc493504def970ceae96f286da19a0af7
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-03-06 20:47:58 -0300

    + Animation

--- CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/AnimationBootcamo.swift
+++ CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/AnimationBootcamo.swift
@@ -23,20 +23,35 @@ struct AnimationBootcamo: View {
             RoundedRectangle(cornerRadius: animacao ? 50 : 30)
                 .fill(animacao  ? Color.yellow : Color.red)
                 .frame(
-                       width: animacao ? 100 :  300,
-                       height: animacao ?  /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/: 450)
+                    width: animacao ? 100 :  300,
+                    height: animacao ?  /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/: 450)
                 .rotationEffect(Angle(degrees: animacao ? 360: 0))
                 .offset(y: animacao ? 300: 0)
                 .animation(Animation
-                .default
-                .repeatForever(autoreverses: true))
-             
+                            .default
+                            .repeatForever(autoreverses: true))
+            
             
             Spacer()
             
+        }
+        
+        VStack{
+            Button("Botão"){
+                withAnimation(.default){
+                    animacao.toggle()}
+            }
+            Spacer()
             
+            RoundedRectangle(cornerRadius: animacao ? 50 : 30)
+                .fill(animacao  ? Color.yellow : Color.red)
+                .frame(
+                    width: animacao ? 100 :  300,
+                    height: animacao ?  /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/: 300)
+            Spacer()
             
         }
+        
     }
 }
 
