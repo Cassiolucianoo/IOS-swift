95f67083a74e31d39091478dd4222cc869fab9f7
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-03-11 20:11:26 -0300

    more about Amination

--- CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/AnimationTimingBootcamp.swift
+++ CursoIOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/AnimationTimingBootcamp.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct AnimationTimingBootcamp: View {
-    @State var animacao: Bool = false
-    let tempo: Double = 1
+    @State var animacao: Bool = true
+    let tempo: Double = 0.50
     
     var body: some View {
+        
         VStack{
             Button("Botão"){
                 animacao.toggle()
@@ -20,8 +21,33 @@ struct AnimationTimingBootcamp: View {
                 .frame(width: animacao ?  350 : 50, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .animation(Animation.linear(duration: tempo))
             
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animacao ?  350 : 50, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .animation(Animation.easeIn(duration: tempo))
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animacao ?  350 : 50, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .animation(Animation.easeOut(duration: tempo))
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animacao ?  350 : 50, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .animation(Animation.easeOut(duration: tempo))
+         
+        }
+        
+        
+        VStack{
+            Button("Botão"){
+                animacao.toggle()
+            }
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animacao ?  350 : 50, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .animation(Animation.linear(duration: tempo))
+         
         }
     }
+  
+    
 }
 
 struct AnimationTimingBootcamp_Previews: PreviewProvider {
