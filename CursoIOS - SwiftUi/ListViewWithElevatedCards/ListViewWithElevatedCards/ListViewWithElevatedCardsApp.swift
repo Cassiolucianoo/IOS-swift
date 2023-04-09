1300c3abdc515b4a8c0568de1f46c5651ba63cdc
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-04-09 11:56:56 -0300

    List view com cards

---
+++ CursoIOS - SwiftUi/ListViewWithElevatedCards/ListViewWithElevatedCards/ListViewWithElevatedCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewWithElevatedCardsApp.swift
+//  ListViewWithElevatedCards
+//
+//  Created by cassio on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ListViewWithElevatedCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+           // ContentView(data: <#ItemData#>)
+        }
+    }
+}
