900a2fb2dd9c316f185db4f5c489fce9f567176b
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-05-17 23:49:31 -0300

    BackgroundMaterials
    recursos visuais oferecidos pelo framework SwiftUI para ajudar a criar interfaces atraentes e consistentes.

---
+++ Curso IOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/BackgroundMaterialsBootcamp.swift
@@ -0,0 +1,38 @@
+//
+//  BackgroundMaterialsBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by cassio on 17/05/23.
+//
+
+import SwiftUI
+
+
+struct BackgroundMaterialsBootcamp: View {
+
+    var body: some View {
+        
+        VStack{
+            Spacer()
+            VStack{
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 50, height: 4)
+                    .padding()
+                Spacer()
+            }
+            .frame(height: 350)
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+            .cornerRadius(30)
+        }
+        .ignoresSafeArea()
+        .background(
+        Image("joel"))
+    }
+}
+
+struct BackgroundMaterialsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundMaterialsBootcamp()
+    }
+}
