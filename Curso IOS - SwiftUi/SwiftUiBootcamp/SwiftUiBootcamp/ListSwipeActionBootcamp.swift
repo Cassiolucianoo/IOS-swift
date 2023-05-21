4c7ea76289874787f3065c15c7402baa4e55d9dc
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-05-21 03:50:04 -0300

    swipeActions
    Você pode definir botões ou ações que serão acionados quando o usuário deslizar o dedo para a direita ou esquerda no elemento.

--- Curso IOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/ListSwipeActionBootcamp.swift
+++ Curso IOS - SwiftUi/SwiftUiBootcamp/SwiftUiBootcamp/ListSwipeActionBootcamp.swift
@@ -10,101 +10,167 @@ import SwiftUI
 struct ListSwipeActionBootcamp: View {
     
     // MARK: VARIAVEIS
-    @State private var frutas = ["maçã", "banana", "laranja"]
+    @State private var frutas = ["01maçã", "02banana", "03laranja","04maçã", "05banana", "06laranja"]
+    @State private var frutasFavoritas = ["00maçã"]
     @State private var exibirViewEdicao = false
     @State private var frutaSelecionada = ""
+    @State private var frutaEditada = ""
+    @State private var indiceFrutaEditada = 0
+    @State private var editando = false
+    @State private var novaFruta = ""
+    
     
     var corFundo2 = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-    
     var body: some View {
         
         NavigationView{
             
             VStack{
                 
-                List {
-                    //ForEach(frutas, id: \.self) {
-                    ForEach(frutas, id: \.self) { fruta in
-                        Text(fruta.capitalized)
-                        // Text($0.capitalized)
-                        //.listRowBackground(Color.red) // Cor de fundo de cada linha
-                            .swipeActions(edge: .trailing, allowsFullSwipe: true){
-                                Button("Delete"){
-                                    //pressionado, chamamos a função deleteFruta e passamos a fruta correspondente como parâmetro. Dentro da função deleteFruta, procuramos o índice da fruta no array
-                                    deleteFruta(fruta)
-                                }
-                                .tint(.red)
-                                Button("Salvar"){
-                                    
-                                }
-                                .tint(.green)
-                                Button("Editar"){
-                                    exibirViewEdicao.toggle()
-                                }
-                                .tint(.blue)
-                                
-                            }
+                List(0..<frutas.count, id: \.self) { fruta in
+                    Button(action: {
+                        indiceFrutaEditada = fruta
+                        frutaEditada = frutas[fruta]
+                        editando = true
+                    }) {
+                        Text(frutas[fruta])
                         
-                            .swipeActions(edge: .leading, allowsFullSwipe: true){
-                                Button("Favoritar"){
-                                    
-                                    frutas.append("banana")
-                                }
-                                .tint(.yellow)
-                                
-                            }
+                    }.swipeActions(edge: .trailing, allowsFullSwipe: true){
+                        Button("Delete"){
+                            //pressionado, chamamos a função deleteFruta e passamos a fruta correspondente como parâmetro. Dentro da função deleteFruta, procuramos o índice da fruta no array
+                            deleteFruta(frutas[fruta])
+                        }
+                        .tint(.red)
+                        Button("Salvar"){
+                            salvar()
+                        }
+                        .tint(.green)
+                        Button("Editar"){
+                            editar(fruta)
+                        }
+                        .tint(.blue)
+                        
                     }
-                    
-                }.sheet(isPresented: $exibirViewEdicao) {
-                   // NovaTelaa(fruta: $frutaSelecionada, listaFrutas: $frutas)
+                    .swipeActions(edge: .leading, allowsFullSwipe: true){
+                        //Neste exemplo, adicionamos um botão que remove a primeira fruta da lista frutas usando frutas.removeFirst(), e em seguida, adicionamos essa fruta à lista frutasFavoritas usando frutasFavoritas.append(primeiraFruta).
+                        Button("Favoritar"){
+                            if let primeiraFruta = frutas.first {
+                                frutas.removeFirst()
+                                frutasFavoritas.append(primeiraFruta)}
+                        }
+                        .tint(.yellow)
+                    }
                 }
-                
-                
                 .navigationTitle("FRUTAS")
-                
+                .navigationBarItems(trailing:
+                                        HStack {
+                    TextField("Nova Fruta", text: $novaFruta)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button(action: {
+                        adicionarFruta()
+                        
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }
+                )
+                //MARK: - FAVORITOS
                 Text("Favoritos")
                     .font(.headline)
                     .foregroundColor(.yellow)
                 
                 List {
-                    ForEach(frutas, id: \.self) {
-                        Text($0.capitalized)
+                    ForEach(frutasFavoritas, id: \.self) {fruta in
+                        Text(fruta.capitalized)
                         
                     }
-                    
                 }
-                
             }    .listStyle(PlainListStyle())
                 .background(Color(corFundo2)) // Cor de fundo da lista
             
         }
+        //MARK: - FAVORITOS fim
+        
+        //MARK: - EDITAR INICIO
+        
+        .sheet(isPresented: $editando) {
+            
+            VStack {
+                
+                Text("Editar fruta da lista")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.black)
+                
+                TextField("Editar Fruta", text: $frutaEditada)
+                    .padding()
+                    .background(Color.gray.opacity(0.2).cornerRadius(10))
+                    .font(.headline)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 1))
+                
+                Button(action: {
+                    frutas[indiceFrutaEditada] = frutaEditada
+                    editando = false
+                }) {
+                    Text("Salvar")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(10)
+                        .foregroundColor(.black)
+                        .font(.headline)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 2))
+                }
+            }.padding()
+                .presentationDetents([.large, .medium, .fraction(0.3)])
+        }
+        
+        //MARK: - EDITAR fim
+        
     }
     //usando firstIndex(of:) e removemos a fruta do array.
+    
+    func adicionarFruta() {
+        if !novaFruta.isEmpty {
+            frutas.append(novaFruta)
+            novaFruta = ""
+        }
+    }
+    
     func deleteFruta(_ fruta: String) {
         if let index = frutas.firstIndex(of: fruta) {
             frutas.remove(at: index)
         }
     }
-    func add(indexSet: IndexSet){
-        
-    }
-    func editar(indexSet: IndexSet){
-        
+    func editar(_ fruta: Int) {
+        indiceFrutaEditada = fruta
+        frutaEditada = frutas[fruta]
+        editando = true
     }
     func favoritar(indexSet: IndexSet){
         
     }
     
+    func salvar() {
+        frutas[indiceFrutaEditada] = frutaEditada
+        editando = false
+    }
+    
 }
 
-struct NovaTelaa: View {
+struct Editar: View {
+    
+    var salvarFruta: () -> Void
+    
     @Binding var fruta: String
- 
-
+    @Binding var editandoFruta: Bool
+    
     var corFundo2 = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     //discartando uma tela
     @Environment(\.presentationMode)  var presentationMode
-   
     
     var body: some View {
         
@@ -129,36 +195,32 @@ struct NovaTelaa: View {
             .background(Color.orange)
             
             VStack(){
-            
-            Text("Editar fruta da lista")
-                .padding()
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            
+                
+                
+                Text("Editar fruta da lista")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.red)
                 
                 TextField("Fruta", text: $fruta)
-                .padding()
-                .background(Color.gray.opacity(0.3).cornerRadius(10))
-                .font(.headline)
-             
-            Button(action: {
-              
-
-                
-            }, label: {
-                Text("Salvar")
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .cornerRadius(10)
-                    .foregroundColor(.black)
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
                     .font(.headline)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.black, lineWidth: 2)
-                    )
                 
-            })
-            
-            
+                Button(action: {
+                    
+                }, label: {
+                    Text("Salvar")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(10)
+                        .foregroundColor(.black)
+                        .font(.headline)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                })
+                
                 Color( corFundo2)
                     .ignoresSafeArea(.all)
                 
