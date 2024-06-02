a47c1098b6a4c3548ab06e4c44d33fc9d90bae64
cassio luciano da silva <cassiolucianodasilva@gmail.com>
2024-06-01 22:11:22 -0300

    Organização e conteudo novo de treinamento em aula

---
+++ Curso IOS 11 - criando 20 Apps/Treinamento - Botoes/Treinamento - Botoes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treinamento - Botoes
+//
+//  Created by cassio on 01/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Curso IOS 11 - criando 20 Apps/Treinamento - TextFild/Treinamento - TextFild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treinamento - TextFild
+//
+//  Created by cassio on 01/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
