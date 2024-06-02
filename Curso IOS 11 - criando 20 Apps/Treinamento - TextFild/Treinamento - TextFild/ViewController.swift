6780e7b07f197aed9f3e9d02a95f5b93fa5808ac
cassio luciano da silva <cassiolucianodasilva@gmail.com>
2024-06-02 09:03:34 -0300

    UIKit TextFild personalizados e Delegate

--- Curso IOS 11 - criando 20 Apps/Treinamento - TextFild/Treinamento - TextFild/ViewController.swift
+++ Curso IOS 11 - criando 20 Apps/Treinamento - TextFild/Treinamento - TextFild/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tfName.delegate = self
+        tfEmail.delegate = self
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    //fechar teclado ao apertar na view
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
-
+  //Next Delegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == tfName {
+            tfEmail.becomeFirstResponder()
+        }else if textField == tfEmail{
+            print("Enviando dados para servidor")
+           // tfPassword.becomeFirstResponder()
+            tfPassword.resignFirstResponder()
+        }
+    return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return !textField.text!.isEmpty
+    }
 }
+
+
 
